71de6e7d7615a74c17b6ebc7a510967952bd05a1
luca.priftis@outlook.com <luca.priftis@outlook.com>
2023-11-12 18:46:25 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Calculadora
+{
+    public int Somar(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtrair(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Dividir(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Não é possível dividir por zero.");
+        }
+        return a / b;
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var calculadora = new Calculadora();
+
+        Console.WriteLine("Calculadora");
+
+        while (true)
+        {
+            Console.WriteLine("Escolha uma operação:");
+            Console.WriteLine("1. Somar");
+            Console.WriteLine("2. Subtrair");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("5. Sair");
+
+            Console.Write("Digite sua escolha (1-5): ");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    PerformOperation(calculadora.Somar, "Somar");
+                    break;
+                case "2":
+                    PerformOperation(calculadora.Subtrair, "Subtrair");
+                    break;
+                case "3":
+                    PerformOperation(calculadora.Multiplicar, "Multiplicar");
+                    break;
+                case "4":
+                    PerformOperation(calculadora.Dividir, "Dividir");
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Escolha inválida. Por gentileza pressiona um número entre 1 e 5.");
+                    break;
+            }
+        }
+    }
+
+    static void PerformOperation(Func<int, int, int> operacao, string NomeOperacao)
+    {
+        Console.Write($"Insira o primeiro número para {NomeOperacao}: ");
+        var num1 = int.Parse(Console.ReadLine());
+
+        Console.Write($"Insira o segundo número para {NomeOperacao}: ");
+        var num2 = int.Parse(Console.ReadLine());
+
+        try
+        {
+            var resultado = operacao(num1, num2);
+            Console.WriteLine($"{NomeOperacao} resultado: {resultado}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
